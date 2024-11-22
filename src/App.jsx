3700339283142647db11986649546d9c94a8477b
AhsanAhmad3 <bscs21014@itu.edu.pk>
2024-11-23 00:27:45 +0500

    Places

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
 
           <Route path="/account/:subpage?" element={<AccountPage />} />
       
+          <Route path="/account/:subpage/:action" element={<AccountPage />} />
           
 
         </Route>
